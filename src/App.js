0349aac4bae58c1707f12ecf352e43e117f9190a
benpaquier <hello@benpaquier.com>
2021-08-20 15:56:23 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from 'styled-components'
+import socketIOClient from "socket.io-client"
+
+import { Button, Input } from 'antd';
+import 'antd/dist/antd.css';
+
+const ENDPOINT = "http://127.0.0.1:4001";
+
+const Footer = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding: 10px;
+  border: solid grey 1px;
+`
+
+const Form = styled.div`
+  display: flex;
+  width: 100%;
+`
+
 
 function App() {
+  const [message, setMessage] = useState(null)
+  const [messages, setMessages] = useState([])
+  let socket
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT, {
+      withCredentials: true
+    })
+
+    socket.on("message received", message => {
+      console.log(message)
+    })
+  }, []);
+  
+  const handleSubmit = () => {
+    message && socket.emit("message submitted", message)
+  }
+
+  const handleChange = (e) => {
+    setMessage(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Footer>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          autoFocus
+          onChange={handleChange}
+        />
+        <Button type="submit">
+          Envoyer
+        </Button>
+      </Form>
+    </Footer>
   );
 }
 
